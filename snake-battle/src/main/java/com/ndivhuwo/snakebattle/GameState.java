9586fc7aca92253709e2514e1e399ef70c8ef57b
23026475 <ndivhuswiswi@gmail.com>
2026-01-27 20:25:39 +0200

    Add GameState grid and food tracking

---
+++ snake-battle/src/main/java/com/ndivhuwo/snakebattle/GameState.java
@@ -0,0 +1,50 @@
+package com.ndivhuwo.snakebattle;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameState {
+
+    private final int width;
+    private final int height;
+
+    private final Set<Position> foodPositions = new HashSet<>();
+
+    public GameState(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int width() {
+        return width;
+    }
+
+    public int height() {
+        return height;
+    }
+
+    public Set<Position> foodPositions() {
+        return foodPositions;
+    }
+
+    public boolean isInsideGrid(Position position) {
+        return position.x() >= 0 &&
+                position.y() >= 0 &&
+                position.x() < width &&
+                position.y() < height;
+    }
+
+    public boolean hasFoodAt(Position position) {
+        return foodPositions.contains(position);
+    }
+
+    public void addFood(Position position) {
+        if (isInsideGrid(position)) {
+            foodPositions.add(position);
+        }
+    }
+
+    public void removeFood(Position position) {
+        foodPositions.remove(position);
+    }
+}
