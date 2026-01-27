b535ffc9190c4fcc4eb499ed94b1b4b05199191c
23026475 <ndivhuswiswi@gmail.com>
2026-01-27 19:50:57 +0200

    Add base package and SnakeBattle entry point

---
+++ snake-battle/src/main/java/com/ndivhuwo/snakebattle/SnakeBattle.java
@@ -0,0 +1,9 @@
+package com.ndivhuwo.snakebattle;
+
+public class SnakeBattle {
+
+    public static void main(String[] args) {
+        System.out.println("🐍 Snake Battle initialized");
+        System.out.println("Environment is set up correctly.");
+    }
+}
