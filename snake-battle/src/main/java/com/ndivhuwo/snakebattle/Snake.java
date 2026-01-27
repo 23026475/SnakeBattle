68606f1cea14a0137c6c9e7d05656b367fdb4da7
23026475 <ndivhuswiswi@gmail.com>
2026-01-27 21:21:59 +0200

    added stats

--- snake-battle/src/main/java/com/ndivhuwo/snakebattle/Snake.java
+++ snake-battle/src/main/java/com/ndivhuwo/snakebattle/Snake.java
@@ -7,6 +7,9 @@ public class Snake {
 
     private final Deque<Position> body = new LinkedList<>();
     private boolean alive = true;
+    private int foodEaten = 0;
+    private int snakesEaten = 0;
+    private int turnsSurvived = 0;
 
     public Snake(Position startPosition) {
         body.addFirst(startPosition);
@@ -27,7 +30,29 @@ public class Snake {
     public void kill() {
         this.alive = false;
     }
+    public void incrementFoodEaten() {
+        foodEaten++;
+    }
 
+    public void incrementSnakesEaten() {
+        snakesEaten++;
+    }
+
+    public void incrementTurnsSurvived() {
+        turnsSurvived++;
+    }
+
+    public int getFoodEaten() {
+        return foodEaten;
+    }
+
+    public int getSnakesEaten() {
+        return snakesEaten;
+    }
+
+    public int getTurnsSurvived() {
+        return turnsSurvived;
+    }
     /**
      * Moves the snake one step in the given direction.
      * If grow is false, the tail is removed.
