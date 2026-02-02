2403bc6a793e7a298ebb8e18a3fe63970ac5d0fb
23026475 <ndivhuswiswi@gmail.com>
2026-02-02 13:08:04 +0200

    Initial commit for SnakeBattle: launch GameFrame

--- snake-battle/src/main/java/com/ndivhuwo/snakebattle/SnakeBattle.java
+++ snake-battle/src/main/java/com/ndivhuwo/snakebattle/SnakeBattle.java
@@ -1,25 +1,14 @@
 package com.ndivhuwo.snakebattle;
 
+import com.ndivhuwo.snakebattle.ui.GameFrame;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class SnakeBattle {
 
+
     public static void main(String[] args) {
-        JPanel redpanel = new JPanel();
-        JPanel bluepanel = new JPanel();
-        JPanel pinkpanel = new JPanel();
-
-        redpanel.setBackground(Color.red);
-        redpanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        redpanel.setBounds(1, 1, 245,245);
-
-        bluepanel.setBackground(Color.green);
-        bluepanel.setBorder(BorderFactory.createLineBorder(Color.white));
-        bluepanel.setBounds(0, 245, 245, 245);
-
-        pinkpanel.setBackground(Color.pink);
-        pinkpanel.setBorder(BorderFactory.createLineBorder(Color.black));
-        pinkpanel.setBounds(0, 245, 245, 245);
+        new GameFrame();
     }
 }
