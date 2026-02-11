ac4502618ec75bfe2b54a52351ce31cebba1873d
23026475 <ndivhuswiswi@gmail.com>
2026-02-11 07:22:52 +0200

    Fixed timer and focus management issues

--- snake-battle/src/main/java/com/ndivhuwo/snakebattle/SnakeGame.java
+++ snake-battle/src/main/java/com/ndivhuwo/snakebattle/SnakeGame.java
@@ -1,27 +1,58 @@
 package com.ndivhuwo.snakebattle;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class SnakeGame extends JFrame {
+
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+    private GamePanel gamePanel;
+    private GameMenu gameMenu;
 
     public SnakeGame() {
         initUI();
     }
 
     private void initUI() {
-        add(new GamePanel());
-
-        setTitle("Classic Snake Game");
+        setTitle("Snake Game with DFS");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        gameMenu = new GameMenu(this);
+        gamePanel = new GamePanel(this);
+
+        mainPanel.add(gameMenu, "MENU");
+        mainPanel.add(gamePanel, "GAME");
+
+        add(mainPanel);
+
         pack();
         setLocationRelativeTo(null);
     }
 
+    public void startGame(String mode) {
+        gamePanel.setGameMode(mode);
+        gamePanel.startGame();
+        cardLayout.show(mainPanel, "GAME");
+        gamePanel.requestFocusInWindow();
+    }
+
+    public void showMenu() {
+        cardLayout.show(mainPanel, "MENU");
+    }
+
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
-            JFrame game = new SnakeGame();
-            game.setVisible(true);
+        // Properly start on the Event Dispatch Thread
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                SnakeGame game = new SnakeGame();
+                game.setVisible(true);
+            }
         });
     }
 }
